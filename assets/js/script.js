8d7b77df9b2ab3bbe40724fec84a09226f600f09
Rohan handore <rohanhandore021@gmail.com>
2024-06-24 22:11:49 +0100

    added baground closes #2

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,20 +1,18 @@
-'use strict';
-
-
+"use strict";
 
 // element toggle function
-const elementToggleFunc = function (elem) { elem.classList.toggle("active"); }
-
-
+const elementToggleFunc = function (elem) {
+	elem.classList.toggle("active");
+};
 
 // sidebar variables
 const sidebar = document.querySelector("[data-sidebar]");
 const sidebarBtn = document.querySelector("[data-sidebar-btn]");
 
 // sidebar toggle functionality for mobile
-sidebarBtn.addEventListener("click", function () { elementToggleFunc(sidebar); });
-
-
+sidebarBtn.addEventListener("click", function () {
+	elementToggleFunc(sidebar);
+});
 
 // testimonials variables
 const testimonialsItem = document.querySelectorAll("[data-testimonials-item]");
@@ -29,31 +27,29 @@ const modalText = document.querySelector("[data-modal-text]");
 
 // modal toggle function
 const testimonialsModalFunc = function () {
-  modalContainer.classList.toggle("active");
-  overlay.classList.toggle("active");
-}
+	modalContainer.classList.toggle("active");
+	overlay.classList.toggle("active");
+};
 
 // add click event to all modal items
 for (let i = 0; i < testimonialsItem.length; i++) {
+	testimonialsItem[i].addEventListener("click", function () {
+		modalImg.src = this.querySelector("[data-testimonials-avatar]").src;
+		modalImg.alt = this.querySelector("[data-testimonials-avatar]").alt;
+		modalTitle.innerHTML = this.querySelector(
+			"[data-testimonials-title]"
+		).innerHTML;
+		modalText.innerHTML = this.querySelector(
+			"[data-testimonials-text]"
+		).innerHTML;
 
-  testimonialsItem[i].addEventListener("click", function () {
-
-    modalImg.src = this.querySelector("[data-testimonials-avatar]").src;
-    modalImg.alt = this.querySelector("[data-testimonials-avatar]").alt;
-    modalTitle.innerHTML = this.querySelector("[data-testimonials-title]").innerHTML;
-    modalText.innerHTML = this.querySelector("[data-testimonials-text]").innerHTML;
-
-    testimonialsModalFunc();
-
-  });
-
+		testimonialsModalFunc();
+	});
 }
 
 // add click event to modal close button
 modalCloseBtn.addEventListener("click", testimonialsModalFunc);
 overlay.addEventListener("click", testimonialsModalFunc);
-
-
 
 // custom select variables
 const select = document.querySelector("[data-select]");
@@ -61,59 +57,49 @@ const selectItems = document.querySelectorAll("[data-select-item]");
 const selectValue = document.querySelector("[data-selecct-value]");
 const filterBtn = document.querySelectorAll("[data-filter-btn]");
 
-select.addEventListener("click", function () { elementToggleFunc(this); });
+select.addEventListener("click", function () {
+	elementToggleFunc(this);
+});
 
 // add event in all select items
 for (let i = 0; i < selectItems.length; i++) {
-  selectItems[i].addEventListener("click", function () {
-
-    let selectedValue = this.innerText.toLowerCase();
-    selectValue.innerText = this.innerText;
-    elementToggleFunc(select);
-    filterFunc(selectedValue);
-
-  });
+	selectItems[i].addEventListener("click", function () {
+		let selectedValue = this.innerText.toLowerCase();
+		selectValue.innerText = this.innerText;
+		elementToggleFunc(select);
+		filterFunc(selectedValue);
+	});
 }
 
 // filter variables
 const filterItems = document.querySelectorAll("[data-filter-item]");
 
 const filterFunc = function (selectedValue) {
-
-  for (let i = 0; i < filterItems.length; i++) {
-
-    if (selectedValue === "all") {
-      filterItems[i].classList.add("active");
-    } else if (selectedValue === filterItems[i].dataset.category) {
-      filterItems[i].classList.add("active");
-    } else {
-      filterItems[i].classList.remove("active");
-    }
-
-  }
-
-}
+	for (let i = 0; i < filterItems.length; i++) {
+		if (selectedValue === "all") {
+			filterItems[i].classList.add("active");
+		} else if (selectedValue === filterItems[i].dataset.category) {
+			filterItems[i].classList.add("active");
+		} else {
+			filterItems[i].classList.remove("active");
+		}
+	}
+};
 
 // add event in all filter button items for large screen
 let lastClickedBtn = filterBtn[0];
 
 for (let i = 0; i < filterBtn.length; i++) {
+	filterBtn[i].addEventListener("click", function () {
+		let selectedValue = this.innerText.toLowerCase();
+		selectValue.innerText = this.innerText;
+		filterFunc(selectedValue);
 
-  filterBtn[i].addEventListener("click", function () {
-
-    let selectedValue = this.innerText.toLowerCase();
-    selectValue.innerText = this.innerText;
-    filterFunc(selectedValue);
-
-    lastClickedBtn.classList.remove("active");
-    this.classList.add("active");
-    lastClickedBtn = this;
-
-  });
-
+		lastClickedBtn.classList.remove("active");
+		this.classList.add("active");
+		lastClickedBtn = this;
+	});
 }
-
-
 
 // contact form variables
 const form = document.querySelector("[data-form]");
@@ -122,19 +108,15 @@ const formBtn = document.querySelector("[data-form-btn]");
 
 // add event to all form input field
 for (let i = 0; i < formInputs.length; i++) {
-  formInputs[i].addEventListener("input", function () {
-
-    // check form validation
-    if (form.checkValidity()) {
-      formBtn.removeAttribute("disabled");
-    } else {
-      formBtn.setAttribute("disabled", "");
-    }
-
-  });
+	formInputs[i].addEventListener("input", function () {
+		// check form validation
+		if (form.checkValidity()) {
+			formBtn.removeAttribute("disabled");
+		} else {
+			formBtn.setAttribute("disabled", "");
+		}
+	});
 }
-
-
 
 // page navigation variables
 const navigationLinks = document.querySelectorAll("[data-nav-link]");
@@ -142,18 +124,115 @@ const pages = document.querySelectorAll("[data-page]");
 
 // add event to all nav link
 for (let i = 0; i < navigationLinks.length; i++) {
-  navigationLinks[i].addEventListener("click", function () {
-
-    for (let i = 0; i < pages.length; i++) {
-      if (this.innerHTML.toLowerCase() === pages[i].dataset.page) {
-        pages[i].classList.add("active");
-        navigationLinks[i].classList.add("active");
-        window.scrollTo(0, 0);
-      } else {
-        pages[i].classList.remove("active");
-        navigationLinks[i].classList.remove("active");
-      }
-    }
-
-  });
+	navigationLinks[i].addEventListener("click", function () {
+		for (let i = 0; i < pages.length; i++) {
+			if (this.innerHTML.toLowerCase() === pages[i].dataset.page) {
+				pages[i].classList.add("active");
+				navigationLinks[i].classList.add("active");
+				window.scrollTo(0, 0);
+			} else {
+				pages[i].classList.remove("active");
+				navigationLinks[i].classList.remove("active");
+			}
+		}
+	});
 }
+
+// contact js
+
+particlesJS("particles-js", {
+	particles: {
+		number: {
+			value: 100,
+			density: {
+				enable: true,
+				value_area: 1000,
+			},
+		},
+		color: {
+			value: ["#aa73ff", "#f8c210", "#83d238", "#33b1f8"],
+		},
+
+		shape: {
+			type: "circle",
+			stroke: {
+				width: 0,
+				color: "#fff",
+			},
+			polygon: {
+				nb_sides: 5,
+			},
+			image: {
+				src: "img/github.svg",
+				width: 100,
+				height: 100,
+			},
+		},
+		opacity: {
+			value: 0.6,
+			random: false,
+			anim: {
+				enable: false,
+				speed: 1,
+				opacity_min: 0.1,
+				sync: false,
+			},
+		},
+		size: {
+			value: 2,
+			random: true,
+			anim: {
+				enable: false,
+				speed: 40,
+				size_min: 0.1,
+				sync: false,
+			},
+		},
+		line_linked: {
+			enable: true,
+			distance: 120,
+			color: "#ffffff",
+			opacity: 0.4,
+			width: 1,
+		},
+	},
+	interactivity: {
+		detect_on: "canvas",
+		events: {
+			onhover: {
+				enable: true,
+				mode: "grab",
+			},
+			onclick: {
+				enable: false,
+			},
+			resize: true,
+		},
+		modes: {
+			grab: {
+				distance: 140,
+				line_linked: {
+					opacity: 1,
+				},
+			},
+			bubble: {
+				distance: 400,
+				size: 40,
+				duration: 2,
+				opacity: 8,
+				speed: 3,
+			},
+			repulse: {
+				distance: 200,
+				duration: 0.4,
+			},
+			push: {
+				particles_nb: 4,
+			},
+			remove: {
+				particles_nb: 2,
+			},
+		},
+	},
+	retina_detect: true,
+});
